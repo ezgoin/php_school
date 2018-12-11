f50ee1d32d5fb0fb837c65c6d33bbcf1f6e79968
Ivan Z <zvaryka.ivan@pdffiller.team>
2018-12-11 08:54:21 +0200

    task 2 completed

--- atm.php
+++ atm.php
@@ -1,29 +1,40 @@
 <?php
 
-$amount = 2750;
+$amount = getopt("a:");
+
 $notes[500] = 0;
 $notes[200] = 0;
 $notes[100] = 0;
 $notes[50] = 0;
 
-while ($amount >= 500) {
-    $amount -= 500;
-    $notes[500]++;
+if ($amount > 0 && $amount < 100000 && ($amount % 50 === 0)) {
+    while ($amount >= 500) {
+        $amount -= 500;
+        $notes[500]++;
+    }
+
+    while ($amount >= 200) {
+        $amount -= 200;
+        $notes[200]++;
+    }
+
+    while ($amount >= 100) {
+        $amount -= 100;
+        $notes[100]++;
+    }
+
+    while ($amount >= 50) {
+        $amount -= 50;
+        $notes[50]++;
+    }
+
+    foreach ($notes as $key => $value) {
+        if ($value !== 0) {
+            echo "$key: $value " . PHP_EOL;
+        }
+    }
+} else {
+
+    echo "Error: incorrect amount. The following notes are available: 50, 100, 200, 500" . PHP_EOL;
 }
 
-while ($amount >= 200) {
-    $amount -= 200;
-    $notes[200]++;
-}
-
-while ($amount >= 100) {
-    $amount -= 100;
-    $notes[100]++;
-}
-
-while ($amount >= 50) {
-    $amount -= 50;
-    $notes[50]++;
-}
-
-print_r($notes);
